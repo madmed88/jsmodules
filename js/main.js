8156d81dcc78b3ce605e6f2ed3907d7a7c1553fb
mbelhadj <mbelhadj@kapit.fr>
2017-05-09 14:14:38 +0100

    step 2: IIFE

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-var input = document.querySelector('#input');
-var form = document.querySelector('#form');
-var resultDiv = document.querySelector('#result');
+(function () {
+    var input = document.querySelector('#input');
+    var form = document.querySelector('#form');
+    var resultDiv = document.querySelector('#result');
 
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    var numbers = input.value
-        .split('+')
-        .map(function(number) {
-            return parseFloat(number)
-        });
-    resultDiv.innerHTML = sum(numbers);
-});
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var numbers = input.value
+            .split('+')
+            .map(function(number) {
+                return parseFloat(number)
+            });
+        resultDiv.innerHTML = sum(numbers);
+    });
+}());
