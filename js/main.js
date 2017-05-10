4380fa3351ee8227c3d70be7c6eb1958566bf492
mbelhadj <mbelhadj@kapit.fr>
2017-05-10 09:57:05 +0100

    step 7: dynamic script loading

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-require(["js/calculator"], function (Calculator) {
-    var input = document.querySelector('#input');
-    var form = document.querySelector('#form');
-    var resultDiv = document.querySelector('#result');
+var httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        var latestVersion = JSON.parse(httpRequest.response)
+            .pop()
+            .ref.split('/')
+            .pop();
+            require(["https://ajax.googleapis.com/ajax/libs/jquery/" + latestVersion + "/jquery.min.js"], function () {
+                start();
+            })
+    }
+};
+httpRequest.open('GET', 'https://api.github.com/repos/jquery/jquery/git/refs/tags');
+httpRequest.send();
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        var numbers = input.value
-            .split('+')
-            .map(function(number) {
-                return parseFloat(number)
-            });
-        resultDiv.innerHTML = Calculator.sum(numbers);
+function start() {
+    require(["js/calculator"], function (Calculator) {
+        var input = $('#input');
+        var form = $('#form');
+        var resultDiv = $('#result');
+
+        form.on('submit', function(event) {
+            event.preventDefault();
+            var numbers = input.val()
+                .split('+')
+                .map(function(number) {
+                    return parseFloat(number)
+                });
+            resultDiv.html(Calculator.sum(numbers));
+        });
     });
-});
+}
